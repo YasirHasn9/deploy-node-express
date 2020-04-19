0cf769d07bd3bf79edc128ffbf720c00f2725953
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-19 11:54:57 -0700

    add message of the day

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+require("dotenv").config();
 const server = require("./api/server.js");
 
-
-// heroku will use the process.env.port to dynamically set it up on their server 
-// so the heroku use .PORT 
+// heroku will use the process.env.port to dynamically set it up on their server
+// so the heroku use .PORT
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
-
 
 // in the package.json heroku is looking under the script object
 // a property called start because this is the default of it.
